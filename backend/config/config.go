35f82403bba3a5bccf97995c96980ad2fdc36dce
y1zhou <zhou.zy.yi@gmail.com>
2019-08-06 10:23:17 -0400

    feat(backend-config): update config json to support sqlite3

--- backend/config/config.go
+++ backend/config/config.go
@@ -6,22 +6,28 @@ import (
 	"log"
 )
 
-// ConfigInfo ...
-type ConfigInfo struct {
-	Database     string `json:"MysqlDatabase"`
-	User         string `json:"MysqlUser"`
-	Password     string `json:"MysqlPassword"`
+// Info ...
+type Info struct {
+	Database struct {
+		Client     string `json:"Client"`
+		Connection struct {
+			User         string `json:"User"`
+			Password     string `json:"Password"`
+			DatabaseName string `json:"DatabaseName"`
+			Filename     string `json:"filename"`
+		} `json:"Connection"`
+	} `json:"Database"`
 	CookieSecret string `json:"CookieSecret"`
 	Port         string `json:"BackendPort"`
 }
 
 // ReadConfigInfo Read config information from a JSON file.
-func ReadConfigInfo(configFile string) ConfigInfo {
+func ReadConfigInfo(configFile string) Info {
 	jsonFile, err := ioutil.ReadFile(configFile)
 	if err != nil {
 		log.Fatal("File '" + configFile + "' not found!\n")
 	}
-	dbInfo := ConfigInfo{}
+	dbInfo := Info{}
 	_ = json.Unmarshal([]byte(jsonFile), &dbInfo)
 	return dbInfo
 }
